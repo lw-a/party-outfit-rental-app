8a2f41007e53380905fed50d87ae67f512231af3
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-08-15 16:39:06 +0900

    made outfit model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_072646) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_073832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "outfits", force: :cascade do |t|
+    t.string "color"
+    t.string "size"
+    t.integer "price"
+    t.text "description"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
