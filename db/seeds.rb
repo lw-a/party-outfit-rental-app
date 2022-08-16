1f53423562afc59836056927322c669926d317be
Adam Hemming <hemming.dev@gmail.com>
2022-08-16 17:14:11 +0900

    add image columns to users&outfits & added to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@
 puts 'Removing the users...'
 User.destroy_all
 puts 'Creating new users...'
-User.create!(email: 'adam@me.com', first_name: 'adam', last_name: '928', password: '123123')
-User.create!(email: 'malene@me.com', first_name: 'malene', last_name: '928', password: '123123')
-User.create!(email: 'andre@me.com', first_name: 'andre', last_name: '928', password: '123123')
-User.create!(email: 'nikki@me.com', first_name: 'nikki', last_name: '928', password: '123123')
+User.create!(email: 'adam@me.com', first_name: 'adam', last_name: '928', password: '123123', avatar: 'https://avatars.githubusercontent.com/u/103736492?v=4')
+User.create!(email: 'malene@me.com', first_name: 'malene', last_name: '928', password: '123123', avatar: 'https://avatars.githubusercontent.com/u/107089457?v=4')
+User.create!(email: 'andre@me.com', first_name: 'andre', last_name: '928', password: '123123', avatar: 'https://avatars.githubusercontent.com/u/102734981?v=4')
+User.create!(email: 'nikki@me.com', first_name: 'nikki', last_name: '928', password: '123123', avatar: 'https://avatars.githubusercontent.com/u/94282878?v=4')
 puts "created #{User.count} users"
 
 
