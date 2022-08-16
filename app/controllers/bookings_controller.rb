960041d6f761a144feaecf431f729779e60adf34
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-16 15:57:06 +0900

    finish create booking action and form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,18 +5,20 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-    @booking.outfit = Outfit.find(params[:outfit_id])
+    @outfit = Outfit.find(params[:outfit_id])
+    @booking.outfit = @outfit
     @booking.user = current_user
+    authorize @booking
     if @booking.save
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      redirect_to outfit_path(@outfit), status: :unprocessable_entity
     end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :comment)
+    params.require(:booking).permit(:start_date, :end_date, :comment, :outfit_id)
   end
 end
