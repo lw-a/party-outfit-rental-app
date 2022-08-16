fa014d3365509c21bf9dd33fe1c18f5619f9488c
Adam Hemming <hemming.dev@gmail.com>
2022-08-16 12:11:24 +0900

    create devise account

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :outfits
 
-  validates :first_name, :last_name, :email, presence: true
+  validates :email, presence: true
+  # :first_name, :last_name,
 end
