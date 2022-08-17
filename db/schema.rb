179fcf2be1066eb70e6b6960a512fa731c826474
Adam Hemming <hemming.dev@gmail.com>
2022-08-17 11:26:22 +0900

    add long and lat to outfits model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_080827) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_022335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_080827) do
     t.string "location"
     t.bigint "user_id"
     t.string "image_url"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_outfits_on_user_id"
   end
 
