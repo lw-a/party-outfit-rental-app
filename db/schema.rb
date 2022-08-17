9d45453c89cc15a533ff1b733e0e6284e2418cdf
Adam Hemming <hemming.dev@gmail.com>
2022-08-17 11:32:56 +0900

    change name of location to address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_022335) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_023203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,7 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_022335) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "location"
+    t.string "address"
     t.bigint "user_id"
     t.string "image_url"
     t.float "latitude"
