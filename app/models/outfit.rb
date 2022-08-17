34547f0d4c329bf2e9eed8bd1942d637bcb6d6d1
Adam Hemming <hemming.dev@gmail.com>
2022-08-17 11:30:44 +0900

    update model

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -3,4 +3,7 @@ class Outfit < ApplicationRecord
   has_one_attached :photo
   has_many :bookings, dependent: :destroy
   validates :color, :size, :location, :category, :name, :description, :price, presence: true
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_address?
 end
