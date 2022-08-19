81e8871946dbd8fc07a5e597c24752f1b62220de
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-19 12:23:23 +0900

    added reviews to show page and repositioned elements

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :outfits, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:create]
+    resources :reviews, only: [:create]
   end
   resources :bookings, only: [:index, :update]
   namespace :owner do
